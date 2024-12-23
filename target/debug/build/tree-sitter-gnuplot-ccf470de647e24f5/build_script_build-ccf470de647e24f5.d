2928c9e9a1054bd2779f5acd2cbbe06d2c92cbc7
Dai López <dpezto@gmail.com>
2024-12-23 07:38:48 -0600

    Revert "Merge pull request #1 from dpezto/release"
    This reverts commit 07ce3740db4c70ad47845f95eb3e32f40582d112, reversing
    changes made to 3c895f5d9c0b3a3c7e02383766b462c21913c000.

--- target/debug/build/tree-sitter-gnuplot-ccf470de647e24f5/build_script_build-ccf470de647e24f5.d
+++
@@ -1,5 +0,0 @@
-/Users/Dpezto/Documents/GitHub/tree-sitter-gnuplot/target/debug/build/tree-sitter-gnuplot-ccf470de647e24f5/build_script_build-ccf470de647e24f5: bindings/rust/build.rs
-
-/Users/Dpezto/Documents/GitHub/tree-sitter-gnuplot/target/debug/build/tree-sitter-gnuplot-ccf470de647e24f5/build_script_build-ccf470de647e24f5.d: bindings/rust/build.rs
-
-bindings/rust/build.rs:
